559ec64bd6304d356dd5dccf26bb77b80cdf5b4f
shehaaz <shehaaz@gmail.com>
2013-02-11 22:25:46 -0500

    working app
    has a tiny bug in collectiondemo though..home button broken

--- gen/com/android/nitelights/R.java
+++ gen/com/android/nitelights/R.java
@@ -14,16 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int demo_collection_button=0x7f070001;
-        public static final int demo_external_activity=0x7f070002;
-        public static final int menu_settings=0x7f070003;
+        public static final int demo_collection_button=0x7f070002;
+        public static final int demo_external_activity=0x7f070003;
+        public static final int menu_settings=0x7f070004;
         public static final int pager=0x7f070000;
+        public static final int pager_title_strip=0x7f070001;
     }
     public static final class layout {
         public static final int activity_collection_demo=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int fragment_section_dummy=0x7f030002;
-        public static final int fragment_section_launchpad=0x7f030003;
+        public static final int fragment_collection_object=0x7f030002;
+        public static final int fragment_section_dummy=0x7f030003;
+        public static final int fragment_section_launchpad=0x7f030004;
     }
     public static final class menu {
         public static final int activity_collection_demo=0x7f060000;
--- src/com/android/nitelights/CollectionDemoActivity.java
+++ src/com/android/nitelights/CollectionDemoActivity.java
@@ -1,22 +1,104 @@
 package com.android.nitelights;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class CollectionDemoActivity extends Activity {
 
+public class CollectionDemoActivity extends FragmentActivity {
+
+	DemoCollectionPagerAdapter mDemocollectionPagerAdapter;
+	
+	ViewPager mViewPager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_collection_demo);
+		
+		mDemocollectionPagerAdapter = new DemoCollectionPagerAdapter(getSupportFragmentManager());
+		
+		final ActionBar actionBar = getActionBar();
+		
+		//show the home button
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		
+		//set up the ViewPager, attaching the adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mDemocollectionPagerAdapter);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_collection_demo, menu);
-		return true;
+	
+	//This hook is called whenever an item in your opetions menu is selected
+	public boolean onOptionItemSelected(MenuItem item){
+		
+		switch(item.getItemId()){
+		case android.R.id.home:
+			//when home is clicked the user is sent to the MainActivity
+			Intent upIntent = new Intent(this, MainActivity.class);
+			if(NavUtils.shouldUpRecreateTask(this, upIntent)) {
+				TaskStackBuilder.create(getApplicationContext());
+				finish();
+			}else {
+				NavUtils.navigateUpTo(this, upIntent);
+			}
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
+	
+	/**
+	 * returns a fragment representing an object in the collection
+	 * @author Lenovo
+	 *
+	 */
+	public static class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
 
+		public DemoCollectionPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+		
+		public int getCount(){
+			return 100;
+		}
+		
+		public CharSequence getPageTitle(int position){
+			return "OBJECT " + (position + 1);
+		}
+
+		@Override
+		public Fragment getItem(int i) {
+			Fragment fragment = new DemoObjectFragment();
+			Bundle args = new Bundle();
+			args.putInt(DemoObjectFragment.ARG_OBJECT, i+1);
+			fragment.setArguments(args);
+			return fragment;
+		}
+	}
+	
+	/**
+	 * a dummy fragment representing a section of the app, but that simply displays dummy text
+	 */
+	public static class DemoObjectFragment extends Fragment {
+		public static final String ARG_OBJECT = "object";
+		
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+			View rootView = inflater.inflate(R.layout.fragment_collection_object, container, false);
+			Bundle args = getArguments();
+			((TextView) rootView.findViewById(android.R.id.text1)).setText(
+					Integer.toString(args.getInt(ARG_OBJECT)));
+			return rootView;
+		}
+	}
 }
